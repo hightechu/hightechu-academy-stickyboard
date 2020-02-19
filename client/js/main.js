a1e75cccac286c140f30e5f90eb29c316c39035e
Jason <jasonyecanada@gmail.com>
2020-02-19 08:33:47 -0800

    Add drawing on canvas

--- client/js/main.js
+++ client/js/main.js
@@ -2,9 +2,45 @@
 
 TogetherJS(document.getElementById("whiteboard"));
 
-// var canvas = document.getElementById("whiteboard");
-// var canvasContext = canvas.getContext("2d");
-// var mousePressed = false;
+var canvas = document.getElementById("whiteboard");
+var canvasContext = canvas.getContext("2d");
 
-// w = canvas.width;
-// h = canvas.height;
+var coord = document.getElementById("coord");
+
+var width = canvas.width;
+var height = canvas.height
+canvasContext.fillStyle(0, 0, 0);
+
+function mouseDown(){
+    mousePressed = true; 
+}
+
+function mouseUp(){
+    mousePressed = false;
+}
+
+function draw(e){
+    var x;
+    var y;
+    if (e.pageX || e.pageY) { 
+        x = e.pageX;
+        y = e.pageY;
+    }
+    else { 
+        x = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft; 
+        y = e.clientY + document.body.scrollTop + document.documentElement.scrollTop; 
+    } 
+    x -= canvas.offsetLeft;
+    y -= canvas.offsetTop;
+
+    coord.innerText = "X: " + x + " || Y: " + y;
+    if(!mousePressed)
+        return;
+
+    canvasContext.beginPath();
+    // canvasContext.rect(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop, 5, 5);
+    // canvasContext.rect(event.pageX - canvas.offsetLeft, event.pageY - canvas.offsetTop, 5, 5);
+    canvasContext.rect(x, y, 5, 5);
+    canvasContext.fill();
+    canvasContext.stroke();
+}
