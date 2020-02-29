571e825a8c4fa1faf577734a4c7f3b4ecdcb5de2
Jason <jasonyecanada@gmail.com>
2020-02-29 13:07:41 -0800

    Add better documentation, clean up code

--- client/js/main.js
+++ client/js/main.js
@@ -1,23 +1,23 @@
-// Add JS Here
-
-// Current Problem: Draw() coordinates are messed up during TogetherJS.send()
-
 TogetherJS(this);
 
+// HTML elements
 var canvas = document.getElementById("whiteboard");
 var canvasContext = canvas.getContext("2d");
 var canvasContainer = document.getElementById("whiteboardContainer");
-var coord = document.getElementById("coord");
+
+// Vars for drawing
 var mousePressed = false;
 var prevCoord;
 var tool = "brush";
 
+////////////////////////////////////
 // Change <div> and <canvas> size //
 
 var width = 1000;
 var height = 600;
 
 //                                //
+////////////////////////////////////
 
 canvasContainer.style.width = String(width) + "px";
 canvasContainer.style.height = String(height) + "px";
@@ -29,6 +29,8 @@ TogetherJS.hub.on("init", function (msg) {
     if (! msg.sameUrl) {
         return;
     }
+
+    // Draws the existing whiteboard onto new client canvas
     var image = new Image();
     image.src = msg.image;
     canvasContext.drawImage(image, 0, 0);
@@ -40,7 +42,7 @@ TogetherJS.hub.on("togetherjs.hello", function(msg){
         return;
     }
     
-    var image = canvas.toDataURL("image/png");
+    var image = canvas.toDataURL("image/png"); // Copy of canvas
     TogetherJS.send({
         type: "init",
         image: image
@@ -52,50 +54,52 @@ TogetherJS.hub.on("draw", function(msg){
     if(!msg.sameUrl){
         return;
     }
-    draw(msg.ex, msg.ey, msg.pex, msg.pey, true);
+    draw(msg.ex, msg.ey, msg.pex, msg.pey, true); // Remote draw() call
 });
 
+// Event listeners
 canvas.addEventListener("mousedown", 
     function(e) { 
-        prevCoord = [e.offsetX, e.offsetY];
+        prevCoord = [e.offsetX, e.offsetY]; // Sets "anchor" for drawing
         mousePressed = mouseDown(); 
     });
 canvas.addEventListener("mouseup", 
     function(e) {
+        // Finish the line
         canvasContext.fill();
         canvasContext.stroke();
         mousePressed = mouseUp(); 
     });
 canvas.addEventListener("mouseleave", function(e) { mousePressed = mouseUp(); })
 canvas.addEventListener("mousemove", 
-    function(e) { 
-        coord.innerText = "X: " + e.offsetX + " || Y: " + e.offsetY;
+    function(e) {
         if(mousePressed){
+            // Call draw() locally
             draw(e.offsetX, e.offsetY, prevCoord[0], prevCoord[1], false); 
 
-            var heo = "heo";
             // Send draw to other clients
             if(TogetherJS.running){
                 TogetherJS.send({
                     type : "draw",
-                    ex : e.offsetX,
-                    ey : e.offsetY,
-                    pex : prevCoord[0],
-                    pey : prevCoord[1]
+                    ex : e.offsetX, // Current X
+                    ey : e.offsetY, // Current Y
+                    pex : prevCoord[0], // Previous X
+                    pey : prevCoord[1] // Previous Y
                 });
             }
         }
-        prevCoord = [e.offsetX, e.offsetY];
+        prevCoord = [e.offsetX, e.offsetY]; // Sets new anchor
     });
 
+// In case the functions need to be expanded
+function mouseUp(){
+    return false;
+}
 function mouseDown(){
     return true;
 }
 
-function mouseUp(){
-    return false;
-}
-
+// Change tool button event handlers
 function brush(){
     tool = "brush";
 }
@@ -103,16 +107,28 @@ function eraser(){
     tool = "eraser";
 }
 
+/* 
+Parameters:
+
+ex : event x (number)
+ey : event y (number)
+pex : previous event x (number)
+pey : previous event y (number)
+remote : if the call is local or from another client (boolean)
+*/
 function draw(ex, ey, pex, pey, remote){
     canvasContext.lineCap = "round";
 
+    // If mouse is up and the call is local, return
     if(!mousePressed && !remote)
         return;
 
+    // Draws a white line, erasing the stroke
     if(tool === "eraser"){
         canvasContext.strokeStyle = "#ffffff";
         canvasContext.lineWidth = 30;
     }
+    // Draws a black line
     else if(tool === "brush"){
         canvasContext.strokeStyle = "#000000";
         canvasContext.lineWidth = 5;
@@ -120,8 +136,8 @@ function draw(ex, ey, pex, pey, remote){
 
     // Begin path, draw line
     canvasContext.beginPath();
-    canvasContext.moveTo(pex, pey);
-    canvasContext.lineTo(ex, ey);
+    canvasContext.moveTo(pex, pey); // Start at anchored point
+    canvasContext.lineTo(ex, ey); // Move to current mouse position
     canvasContext.fill();
     canvasContext.stroke();
 }
