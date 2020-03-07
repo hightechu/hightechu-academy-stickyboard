aeddb27988d231815da5ae0368a18d097a50a53b
Lauro do Couto Guimaraes Teixeira <laurod@uvic.ca>
2020-03-07 11:17:44 -0800

    added color change and width change

--- client/js/main.js
+++ client/js/main.js
@@ -1,54 +1,148 @@
-// Add JS Here
-
-TogetherJS(document.getElementById("whiteboard"));
-
+// HTML elements
 var canvas = document.getElementById("whiteboard");
 var canvasContext = canvas.getContext("2d");
 var canvasContainer = document.getElementById("whiteboardContainer");
-var coord = document.getElementById("coord");
+
+// Vars for drawing
 var mousePressed = false;
 var prevCoord;
 var tool = "brush";
 
-// Change <div> and <canvas> size //
+var mx;
+var my;
 
-var width = 1000;
-var height = 600;
+// Vars for resizing adjustment
+var startingWidth = window.innerWidth;
+var startingHeight = window.innerHeight;
+var currentWidth = startingWidth;
+var currentHeight = startingHeight;
+var startingOffLeft = canvas.offsetLeft;
+var startingOffTop = canvas.offsetTop;
 
-//                                //
+// Set canvas width
+canvas.width  = canvas.offsetWidth;
+canvas.height = canvas.offsetHeight;
 
-canvasContainer.style.width = String(width) + "px";
-canvasContainer.style.height = String(height) + "px";
-canvas.width = width;
-canvas.height = height;
+// Receive canvas image on join
+TogetherJS.hub.on("init", function (msg) {
+    if (! msg.sameUrl) {
+        return;
+    }
 
+    // Draws the existing whiteboard onto new client canvas
+    var image = new Image();   
+    image.src = msg.image;
+    canvasContext.drawImage(image, 0, 0);
+});
+
+// Send canvas image on client join
+TogetherJS.hub.on("togetherjs.hello", function(msg){
+    if(!msg.sameUrl){
+        return;
+    }
+    
+    var image = canvas.toDataURL("image/png"); // Copy of canvas
+    TogetherJS.send({
+        type: "init",
+        image: image
+    });
+});
+
+// Receive command to draw from other client
+TogetherJS.hub.on("draw", function(msg){
+    if(!msg.sameUrl){
+        return;
+    }
+    draw(msg.ex, msg.ey, msg.pex, msg.pey, true); // Remote draw() call
+});
+
+// Clear board from other clients
+TogetherJS.hub.on("clear", function(msg){
+    if(!msg.sameUrl){
+        return;
+    }
+    canvasContext.clearRect(0, 0, canvas.width, canvas.height)
+});
+
+// Change Canvas on screen size change
+window.addEventListener("resize", 
+    function(e){ 
+        currentWidth = window.innerWidth; 
+        currentHeight = window.innerHeight; 
+    });
+
+// Event listeners
 canvas.addEventListener("mousedown", 
     function(e) { 
-        prevCoord = [e.offsetX, e.offsetY];
+        mousePos(e);
+        prevCoord = [mx, my]; // Sets "anchor" for drawing
         mousePressed = mouseDown(); 
     });
 canvas.addEventListener("mouseup", 
     function(e) {
+        // Finish the line
         canvasContext.fill();
         canvasContext.stroke();
         mousePressed = mouseUp(); 
     });
-canvas.addEventListener("mouseleave", function(e) { mousePressed = mouseUp(); })
-canvas.addEventListener("mousemove", function(e) { draw(e, mousePressed, prevCoord); });
+
+canvas.addEventListener("mouseleave", 
+    function(e) {
+        mousePos(e);
+        if(mousePressed){
+            // Call draw() locally
+            draw(mx, my, prevCoord[0], prevCoord[1], false); 
+
+            // Send draw to other clients
+            if(TogetherJS.running){
+                TogetherJS.send({
+                    type : "draw",
+                    ex : mx, // Current X
+                    ey : my, // Current Y
+                    pex : prevCoord[0], // Previous X
+                    pey : prevCoord[1] // Previous Y
+                });
+            }
+        }
+        prevCoord = [mx, my]; // Sets new anchor
+        mousePressed = false;
+    })
+canvas.addEventListener("mousemove", 
+    function(e) {
+        mousePos(e);
+        if(mousePressed){
+            // Call draw() locally
+            draw(mx, my, prevCoord[0], prevCoord[1], false); 
+
+            // Send draw to other clients
+            if(TogetherJS.running){
+                TogetherJS.send({
+                    type : "draw",
+                    ex : mx, // Current X
+                    ey : my, // Current Y
+                    pex : prevCoord[0], // Previous X
+                    pey : prevCoord[1] // Previous Y
+                });
+            }
+        }
+        prevCoord = [mx, my]; // Sets new anchor
+    });
+
 document.getElementById("colors").addEventListener("change", 
     function(e) {
         brush();
     }
 );
 
+// In case the functions need to be expanded
+function mouseUp(){
+    return false;
+}
 function mouseDown(){
     return true;
 }
 
-function mouseUp(){
-    return false;
-}
-
+// Change tool button event handlers
 function brush(){
     tool = "brush";
 }
@@ -56,45 +150,60 @@ function eraser(){
     tool = "eraser";
 }
 
-function getcolor(){
-    
+// Change coords of mouse
+function mousePos(e){
+    mx = (e.pageX * (startingWidth / currentWidth)) - startingOffLeft;
+    my = (e.pageY * (startingHeight / currentHeight)) - startingOffTop;
 }
 
+// Clear canvas
+function clearBoard(){
+    // Double-check
+    if(confirm("Do you want to clear the whiteboard?")){
+        canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+        if(TogetherJS.running){ // Send to other clients
+            TogetherJS.send({
+                type : "clear"
+            });
+        }
+    }
+}
 
-function draw(e, mousePressed, prevCoord){
-    var x = e.offsetX;
-    var y = e.offsetY;
+/* 
+Parameters:
+
+ex : event x (number)
+ey : event y (number)
+pex : previous event x (number)
+pey : previous event y (number)
+remote : if the call is local or from another client (boolean)
+*/
+function draw(ex, ey, pex, pey, remote){
     canvasContext.lineCap = "round";
 
     var getColorPickerByID = document.getElementById("colors").value;
     
     var Width = document.getElementById("width").value;
 
-    coord.innerText = "X: " + x + " || Y: " + y;
-    if(!mousePressed)
-        return prevCoord;
+    // If mouse is up and the call is local, return
+    if(!mousePressed && !remote)
+        return;
 
+    // Draws a white line, erasing the stroke
     if(tool === "eraser"){
         canvasContext.strokeStyle = "#ffffff";
         canvasContext.lineWidth = Width;
     }
-
+    // Draws a black line
     else if(tool === "brush"){
         canvasContext.strokeStyle = getColorPickerByID;
         canvasContext.lineWidth = Width;
     }
-    
-
 
     // Begin path, draw line
     canvasContext.beginPath();
-    canvasContext.moveTo(prevCoord[0], prevCoord[1]);
-    canvasContext.lineTo(x, y);
+    canvasContext.moveTo(pex, pey); // Start at anchored point
+    canvasContext.lineTo(ex, ey); // Move to current mouse position
     canvasContext.fill();
     canvasContext.stroke();
-
-    // Set current coord to old coords
-    prevCoord[0] = x;
-    prevCoord[1] = y;
-    return prevCoord;
 }
